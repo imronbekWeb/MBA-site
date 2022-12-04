b42a58fc7cc8adaeff852f820c49809f0ad99350
imronbekWeb <119215343+imronbekWeb@users.noreply.github.com>
2022-12-04 15:29:26 +0500

    Add files via upload
    MBA site you enter our site

---
+++ mix.js
@@ -0,0 +1,92 @@
+document.addEventListener("DOMContentLoaded", function(e) {
+    const result = document.querySelector(".result"); 
+    const coment = document.getElementById("coment").value;
+    const AddComent = document.getElementById("ButtonComent");
+    const project = document.getElementById("new");
+
+    let direction = [
+      ["MixHost"]
+    ]
+    for(let i = 1; i < direction.length; i++) {
+      if(i.length = 10) {
+        document.lenght = -1;
+        console.log(this.document.lenght);
+        this.result;
+        this.coment;
+        this.AddComent;
+        this.firstChild;
+      }
+    }
+
+    function Add() {
+    AddComent.addEventListener("click", function(e) {
+        result.innerHTML = `
+        <div class="result">
+        <h6 style="font-family: sans-serif;">${document.getElementById("coment").value}</h6>
+        <style>
+          .result {
+            width: 800px;
+            height: 190px;
+            background: #daaacd;
+            display: block;
+            border-radius: 20px;
+            margin-left: 20px;
+          }
+        </style>
+      </div>`;
+    });
+    }
+    Add();
+    return;
+});
+
+function draw() {
+  for(let d = 10; d > 10; d++) {
+    setInterval(() => {
+      let matrix = [
+        [5,6,5,6,7,]
+      ]
+      this.matrix == "";
+      if(matrix > 10 || matrix > d.lenght) {
+        document.addEventListener("keydown", function(e) {
+          HTMLBodyElement.length = -1;
+          HTMLCanvasElement.length = -2;
+        });
+      } 
+    }, 30000);
+  }
+  if(this.document == 2) {
+    console.log(Math.random() + 7);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function(e) {
+  let orange_function = function program() {
+    for (let matrix = 10; matrix > 10; matrix++) {
+      console.log(matrix[10]);
+      matrix++;
+
+      document.doctype = HTMLAllCollection();
+    }
+    let rotate = {
+      rotateY: 45,
+      rotateX: 67,
+    }
+
+    if(rotateY == 18 || rotateX == 20 && rotate != document.lenght -45) {
+console.log(document.lenght);
+    }
+  };
+  const datails = document.getElementById("data");
+  const brauser = document.getElementById("brauser");
+
+  brauser.addEventListener("change", function(e) {
+    console.log(brauser);
+  });
+  
+});
+
+
+
+
+
